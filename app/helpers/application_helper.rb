d676a608cd6d564bdf061f5674c481d43fca0bab
tand1 <tand1@smartosc.com>
2016-03-29 23:16:08 +0900

    Setting Mysql && add view layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Framgia E-learning"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
